bca6315f9073ac1dc90338cb10a53eaed471f637
squinteye1 <neil.d.mckay@googlemail.com>
2022-04-11 23:54:37 +0100

    fetching data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
+import React, { useState, useEffect } from 'react'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
+import Cards from "./components/Cards/Cards";
+import Filters from "./components/Filters/Filters";
 
 function App() {
+  let [pageNumber, setpageNumber] = useState(1);
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
+
+  useEffect(() => {
+    (async function(){
+      let data = await fetch(api).then(res=>res.json());
+      console.log(data);
+    })();
+  }, [api]);
+
   return (
     <div className="App">
       <h1 className="text-center ubuntu my-4">React Project - Rick &amp; <span className="primary">Morty Wiki</span></h1>
       <div className="container">
         <div className="row">
-          <div className="col-3"></div>
-          <div className="col-8"></div>
+          <div className="col-3"><Filters /></div>
+          <div className="col-8">
+            <div className="row">
+              <Cards />
+              <Cards />
+              <Cards />
+              
+            </div>
+          </div>
         </div>
       </div>
     </div>
